47d3c615430e5465ab13eb19c26a89116f992d7c
Ignacio Peralta <145861704+i-peraltam@users.noreply.github.com>
2025-04-04 17:05:18 -0600

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,1 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav>
+      <Link to="/">Inicio</Link> | 
+      <Link to="/dashboard">Dashboard</Link> | 
+      <Link to="/pagos">Pagos</Link> | 
+      <Link to="/reportes">Reportes</Link> | 
+      <Link to="/votaciones">Votaciones</Link>
+    </nav>
+  );
+}
+
+export default Navbar;
 
