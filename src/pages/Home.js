e8537af7f6356026fb47ac6c596408f17335bed8
Ignacio Peralta <145861704+i-peraltam@users.noreply.github.com>
2025-04-04 17:04:52 -0600

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,1 +1,13 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Bienvenido a Los Robles</h1>
+      <p>Esta es la página de inicio del sistema.</p>
+    </div>
+  );
+}
+
+export default Home;
 
