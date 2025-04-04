ce9f4885def44b21ad0ac8a3cb67262cd0c93f76
Ignacio Peralta <145861704+i-peraltam@users.noreply.github.com>
2025-04-04 17:04:23 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,30 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import Pagos from './pages/Pagos';
+import Reportes from './pages/Reportes';
+import Votaciones from './pages/Votaciones';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/pagos" element={<Pagos />} />
+        <Route path="/reportes" element={<Reportes />} />
+        <Route path="/votaciones" element={<Votaciones />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
 
