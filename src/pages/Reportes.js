10d9c3d07d775758fa43e128e3339acea2b07228
Ignacio Peralta <145861704+i-peraltam@users.noreply.github.com>
2025-04-14 15:26:30 -0600

    Update Reportes.js

--- src/pages/Reportes.js
+++ src/pages/Reportes.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import ReportTable from '../components/ReportTable';
+
+const Reportes = () => {
+  return (
+    <div className="container mt-4">
+      <h2>Reportes Financieros</h2>
+      <ReportTable />
+    </div>
+  );
+};
+
+export default Reportes;
 
