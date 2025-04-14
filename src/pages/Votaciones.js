2d40ae5022ec37ac86930eccb6a3d3ee3e1202d4
Ignacio Peralta <145861704+i-peraltam@users.noreply.github.com>
2025-04-14 15:26:46 -0600

    Update Votaciones.js

--- src/pages/Votaciones.js
+++ src/pages/Votaciones.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import ModalVotacion from '../components/ModalVotacion';
+
+const Votaciones = () => {
+  return (
+    <div className="container mt-4">
+      <h2>Sistema de Votaciones y Encuestas</h2>
+      <ModalVotacion />
+    </div>
+  );
+};
+
+export default Votaciones;
 
