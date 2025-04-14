2d9eb13621aa362287b93aeba2adf4b75e687c2e
Ignacio Peralta <145861704+i-peraltam@users.noreply.github.com>
2025-04-14 15:26:09 -0600

    Update Pagos.js

--- src/pages/Pagos.js
+++ src/pages/Pagos.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import CardPago from '../components/CardPago';
+
+const Pagos = () => {
+  return (
+    <div className="container mt-4">
+      <h2>Módulo de Pagos</h2>
+      <CardPago />
+    </div>
+  );
+};
+
+export default Pagos;
 
